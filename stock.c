0d89b84c9022b4cc24101f1723de68d1b7512827
gurramkshethra-droid <gurramkshethra@gmail.com>
2026-01-26 19:01:59 +0530

    Added c practised

---
+++ stock.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+# define MAX 30
+int stack[MAX];
+int top=-1;
+int main() {
+    int ch=1, option;
+    while(ch) {
+        printf("\n enter the choice");
+        printf("\n 1.push  \n 2.pop  \n 3.peek");
+        scanf("%d",&option);
+        switch(option)
+        {
+            case1: printf("\n enter the data");
+            scanf("%d",)
+        }
+    }
+
+}
