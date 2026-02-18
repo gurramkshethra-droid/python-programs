974b049a76a816fdff785b0f51dcb274b5cb9e43
gurramkshethra-droid <gurramkshethra@gmail.com>
2026-02-18 20:28:55 +0530

    Added

--- degrees.py
+++ degrees.py
@@ -1,6 +1,8 @@
 # Enter your code here. Read input from STDIN. Print output to STDOUT
 import math
-ab=int(input())
-ac=int(input())
-angle=round(math.degrees(math.atan(ab/ac)))
-print(str(angle) + "\u00B0")
+
+ab = int(input().strip())
+ac = int(input().strip())
+
+angle = math.degrees(math.atan2(ab, ac))
+print(f"{round(angle)}\u00B0")
