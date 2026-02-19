8e63be4be93be3a27e29338bec4e466602fc79f1
gurramkshethra-droid <gurramkshethra@gmail.com>
2026-02-19 18:19:20 +0530

    Modified

--- set_diff.py
+++ set_diff.py
@@ -1,10 +1,12 @@
-# Enter your code here. Read input from STDIN. Print output to STDOUT
-n=int(input())
-arr=set(map(int,input().split()))
-m=int(input())
-brr=set(map(int,input().split()))
-r1=arr.difference(brr)
-r2=brr.difference(arr)
-result=sorted(r1.union(r2))
-for i in result:
-    print(i)
+# Read input
+n = int(input())
+arr = set(map(int, input().split()))
+
+m = int(input())
+brr = set(map(int, input().split()))
+
+# Symmetric Difference
+result = sorted(arr.symmetric_difference(brr))
+
+# Print result
+print(*result, sep="\n")
