048065f3391a15ed952258ae0f147a178d8f1c9d
gurramkshethra-droid <gurramkshethra@gmail.com>
2026-02-19 18:19:32 +0530

    Modified

--- set_opt.py
+++ set_opt.py
@@ -6,10 +6,11 @@ for _ in range(m):
     cmd = input().split()
     
     if cmd[0] == "pop":
-        arr.remove(min(arr))   # simulate correct pop behavior
+        if arr:   # check if set is not empty
+            arr.pop()
     elif cmd[0] == "remove":
-        arr.remove(int(cmd[1]))
+        arr.remove(int(cmd[1]))   # raises error if not present (as expected)
     elif cmd[0] == "discard":
-        arr.discard(int(cmd[1]))
+        arr.discard(int(cmd[1]))  # does nothing if not present
 
 print(sum(arr))
