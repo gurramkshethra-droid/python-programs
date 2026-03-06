0471469252f91844581a1208e7afb7562b73ad40
gurramkshethra-droid <gurramkshethra@gmail.com>
2026-03-06 22:18:53 +0530

    practised Hackerrank

---
+++ set_dif.py
@@ -0,0 +1,7 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+n=int(input())
+ll1=set(map(int,input().split()))
+m=int(input())
+ll2=set(map(int,input().split()))
+s=ll1.difference(ll2)
+print(len(s))
