fd2f3ae9d323ce051cf6c4b163aabb84df20a949
gurramkshethra-droid <gurramkshethra@gmail.com>
2026-03-08 22:27:23 +0530

    practised Hackerrank

--- calendar_day.py
+++ calendar_day.py
@@ -1,5 +1,5 @@
 import calendar
 month,date,year=map(int,input().split())
-days=calendar.weekday(year,month,date)
+days=calendar.weekday(year,month,date) #using libraries
 weeks=["MONDAY","TUESDAY","WEDNESDAY","THURSDAY","FRIDAY","SATURDAY","SUNDAY"]
 print(weeks[days])
