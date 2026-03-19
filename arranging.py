5c041c812c665e4ef49194d4a175483ec2668c5c
gurramkshethra-droid <gurramkshethra@gmail.com>
2026-03-19 21:21:59 +0530

    Correct logic in arranging.py
    Fix increment of count variable in loop.

--- arranging.py
+++ arranging.py
@@ -5,7 +5,7 @@ result = []
 
 for i in range(1, len(s)):
     if s[i] == s[i-1]:
-        count += 1
+        count += 1#increments
     else:
         result.append(f"({count}, {int(s[i-1])})")
         count = 1
